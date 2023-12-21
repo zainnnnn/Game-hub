a6f98e8969ac8f203075023bab808601162ea908
zainnnnn <mzain2353@gmail.com>
2023-12-21 14:26:05 +0100

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -17,7 +17,12 @@ export interface Game {
   }
   
 const useGames = (gameQuery: GameQuery) => {
-    return useData<Game>("/games",{params:{genres:gameQuery.genre?.id, platforms: gameQuery.platform?.id}}, [gameQuery]);
+    return useData<Game>("/games",
+    {params:{
+      genres:gameQuery.genre?.id,
+      platforms: gameQuery.platform?.id, 
+      ordering: gameQuery.sortOrder}},
+       [gameQuery]);
 }
 
 export default useGames;
